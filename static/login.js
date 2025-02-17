6226f853b35cbfde2fabcb62bf44f13f33f2c906
Saacutter <saacutter3@gmail.com>
2025-02-17 09:43:17 +0800

    Added mobile scaling and adjusted login screen

--- static/login.js
+++ static/login.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Hide the signup panel when the page loads
-    let suPanel = document.querySelector('#signup');
-    suPanel.style.display = "none";
+    let signup = document.querySelector('#signup');
+    signup.style.display = "none";
+
+    // Show the login panel when the page loads
+    let login = document.querySelector('#login');
+    login.style.display = "block";
 
     // Identify the 'signup' button and listen for clicks
     let suButton = document.querySelector('#signup-btn');
     suButton.addEventListener('click', function() {
         // Hide the login panel
-        var login = document.querySelector('#login');
+        disableInputs(login.querySelectorAll('button, input'));
         login.classList.add('fade-out');
         login.style.display = "none"; 
         setTimeout(() => {
@@ -15,21 +19,21 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 2000);
 
         // Show the signup panel
-        var signup = document.querySelector('#signup');
         signup.style.display = "block";
         signup.classList.add('fade-in');
         setTimeout(() => {
             signup.classList.remove('fade-in');
+            enableInputs(signup.querySelectorAll('button, input'));
         }, 2000);
 
         // Update the title to reflect the current view
-        document.title = "CS50 Password | Sign Up";
+        document.title = "Password Manager | Sign Up";
     });
 
     let liButton = document.querySelector('#login-btn');
     liButton.addEventListener('click', function() {
         // Hide the signup panel
-        var signup = document.querySelector('#signup');
+        disableInputs(signup.querySelectorAll('button, input'));
         signup.classList.add('fade-out');
         signup.style.display = "none";
         setTimeout(() => {
@@ -37,14 +41,26 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 2000);
 
         // Show the login panel
-        var login = document.querySelector('#login');
         login.style.display = "block";
         login.classList.add('fade-in');
         setTimeout(() => {
             login.classList.remove('fade-in');
+            enableInputs(login.querySelectorAll('button, input'));
         }, 2000);
 
         // Update the title to reflect the current view
-        document.title = "CS50 Password | Sign In";
+        document.title = "Password Manager | Sign In";
     });
 });
+
+function disableInputs(elements) {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].setAttribute('disabled', true);
+    }
+}
+
+function enableInputs(elements) {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].removeAttribute('disabled');
+    }
+}
